336a324218441e983855f34fb3cd31678e4cb800
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-05-22 20:21:28 -0500

    Update hug.js

--- src/commands/hug.js
+++ src/commands/hug.js
@@ -11,6 +11,12 @@ module.exports = {
  	    "https://i.pinimg.com/originals/85/dc/ef/85dcef131af84b515106955e142df54e.gif",
  	    "https://i.imgur.com/r9aU2xv.gif",
  	    "https://i.pinimg.com/originals/4d/89/d7/4d89d7f963b41a416ec8a55230dab31b.gif"];
+	
+ 	   var hugifs =  [
+	    "https://media.tenor.com/images/b6d0903e0d54e05bb993f2eb78b39778/tenor.gif",
+ 	    "https://i.pinimg.com/originals/85/dc/ef/85dcef131af84b515106955e142df54e.gif",
+ 	    "https://i.imgur.com/r9aU2xv.gif",
+ 	    "https://i.pinimg.com/originals/4d/89/d7/4d89d7f963b41a416ec8a55230dab31b.gif"];		
     
 	    let member = message.mentions.members.first();
 	    if(!member) 
